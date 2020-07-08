4f4a01dd3517cfeed94cd889cdee9dff917c4f5a
Daria Menscikova <menscikova.daria@gmail.com>
2020-07-08 18:12:12 +0200

    added open and close modal item for ul

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,9 @@
 const modalAdd = document.querySelector('.modal__add'),
   btnAdd = document.querySelector('.add__ad'),
   modalBtnSubmit = document.querySelector('.modal__btn-submit'),
-  modalSubmit = document.querySelector('.modal__submit');
+  modalSubmit = document.querySelector('.modal__submit'),
+  catalog = document.querySelector('.catalog'),
+  modalItem = document.querySelector('.modal__item');
 
 btnAdd.addEventListener('click', () => {
   modalAdd.classList.remove('hide');
@@ -19,9 +21,27 @@ modalAdd.addEventListener('click', event => {
     modalSubmit.reset();
   }
 });
-modalAdd.addEventListener('keyup', event => {
-  if(event.key === 27) {
-    modalAdd.classList.add('hide');
-    modalSubmit.reset();
+document.addEventListener('keydown', event => {
+  if(event.keyCode === 27) {
+    if(!modalAdd.classList.contains('hide')){
+      modalAdd.classList.add('hide');
+      modalSubmit.reset();
+    }
+
+  }
+});
+catalog.addEventListener('click', event => {
+  const target = event.target;
+  if(target.closest('li')) {
+    modalItem.classList.remove('hide');
+  }
+
+});
+modalItem.addEventListener('click', event => {
+  const target = event.target;
+
+  if (target.classList.contains('modal__close') ||
+    target === modalItem ) {
+    modalItem.classList.add('hide');
   }
 });
